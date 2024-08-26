8c09cbad7b8d255f809f335cee472339c60449b4
gemacadm <spfbezer@gmail.com>
2024-08-26 09:30:57 -0300

    Revert "Teste inicio.php"
    This reverts commit 00e41848d1c461bee185f2c068a4eb101b1fa48d.

--- inicio.php
+++ inicio.php
@@ -4,9 +4,5 @@
     </head>
     <body>
         <p style="font-size: large;text-align:center;vertical-align:middle;">WEB</p>
-<script>
-alert("teste inicio.php");
-</script>
-
     </body>
 </html>
